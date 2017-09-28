38b334da66dd61b0026c682ec09343d80732c45c
jinzhuming <jinzhuming0308@gmail.com>
2017-09-28 20:58:51 +0800

    添加注册页；

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="main-board">
+    注册
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+
+export default class SignUp extends Vue {
+  msg: string = 'this is a typescript project now'
+}
+</script>
+
+
+<style lang="scss">
+</style>
+
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,14 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Hello from '@/components/Hello.vue'
 import MainBoard from '@/components/MainBoard.vue'
+import SignUp from '@/components/SignUp.vue'
 
 Vue.use(Router)
 const router = new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
+      name: 'MainBoard',
       component: MainBoard,
+    },
+    {
+      path: '/sign_up',
+      name: 'SignUp',
+      component: SignUp,
     },
   ],
 })
